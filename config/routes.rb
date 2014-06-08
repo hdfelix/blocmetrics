b827f30847fd54fd37cd3d8ddfc12e772927feef
Hector D. Felix <hdfelix@gmail.com>
2014-06-07 22:55:25 -0700

    Initial routes file (namespacing, versioning, Events resource); inflections file for API acronym

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
 	match 'events' => "events#index", via: :options
+
+	namespace :api, path: '/', 
+		constraints: { subdomain: 'api' },
+		defaults: { format: :json }do
+		namespace :v1 do
+			resources :events, only: [:create]
+		end
+	end
 end
