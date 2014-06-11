0f366592b70d7cd141ff157fc47a957d8854cc2c
Hector D. Felix <hdfelix@gmail.com>
2014-06-11 16:46:44 -0700

    Added month/year and drop down with referers (not hooked up to anything)

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationsController < ApplicationController
 	def index
 		@events = Event.all
+		@referers = []
+		@all_referers = []
+		@events.all.each do |event|
+			unless event.referer == nil
+				@all_referers << event.referer
+			end
+		end
+
+		@all_referers = @all_referers.uniq
+
+		i = 0
+		@all_referers.each do |referer|
+			@referers << [referer,i]
+			i += 1
+		end
 	end
 end
